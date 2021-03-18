bca8cd0fa81e3419470e672fe1f92450869ab9ca
thonilux <66708349+thonilux@users.noreply.github.com>
2021-03-18 14:54:33 +0700

    data masih dummy tapi bisa

---
+++ HttpClient/upload3kwh.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+  
+const char* ssid = "PRN ICT";
+const char* password =  "prnict2021";
+int i=1;
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  WiFi.begin(ssid, password); 
+  
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+}
+void loop() {
+  
+ if(WiFi.status()== WL_CONNECTED){
+   HTTPClient http;
+   int p = random(0,10);
+   int v = random(220,230);
+   int io = random(0,9);
+   http.begin("https://si.ft.uns.ac.id/smartpowerwall/api/ApiData");
+   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+   String dataout = "kwh="+String(i)+"&power=" + String(p) + "&voltage=" + String(v) + "&current="+ String(io) +"&submit=enter";
+   int httpResponseCode = http.POST(dataout);  
+   Serial.println(httpResponseCode);
+   Serial.println(dataout);
+   http.end();
+   i++;
+   if(i>3){
+    i=1;
+   }
+   Serial.println(dataout);
+   delay(1000);
+ }
+ 
+  else{ 
+    Serial.println("Error in WiFi connection");   
+ }
+}
